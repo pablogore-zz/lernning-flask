d272383d01a9cbf359bfd60782a268983b73a9ae
Pablo Martin Gore <pablo.m.gore@gmail.com>
2017-01-11 01:11:26 -0300

    sasdsad

--- forms.py
+++ forms.py
@@ -1,9 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import DataRequired , Email , Length
 
 class SignupForm(FlaskForm):
-    first_name  =   StringField('First name')
-    last_name   =   StringField('Last name')
-    password    =   PasswordField('Password')
-    email       =   StringField('Email')
+    first_name  =   StringField('First name' , validators=[DataRequired('Please enter your first name')])
+    last_name   =   StringField('Last name'  , validators=[DataRequired('Please enter your last  name')])
+    password    =   PasswordField('Password' , validators=[DataRequired('Please enter your password'),
+                                                           Length(min=6 , message='Password must be 6 character or more.')])
+    email       =   StringField('Email'      , validators=[DataRequired('Please enter your email') ,
+                                                           Email('Enter your valid email')])
     submit      =   SubmitField('Sign up')
--- routes.py
+++ routes.py
@@ -1,9 +1,9 @@
 from flask      import Flask , render_template , request
-from  models    import db
+from  models    import db , User
 from forms      import SignupForm
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/jlt'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://jlt:secret@localhost/jltdb"'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.secret_key = 'development-key'
 db.init_app(app)
@@ -20,9 +20,16 @@ def about():
 def singup():
     form = SignupForm()
     if request.method == 'POST':
-        return 'Succesfully!!!'
+        if form.validate()==False:
+            return render_template('signup.html' , form=form)
+        else:
+            newuser = User(form.first_name.data , form.last_name.data , form.email.data , form.password.data)
+            db.session.add(newuser)
+            db.session.commit()
+            return 'Succesfully!!!'
     elif request.method=='GET':
         return render_template('signup.html' , form = form)
 
 if __name__ == '__main__':
+
     app.run(debug=True)
